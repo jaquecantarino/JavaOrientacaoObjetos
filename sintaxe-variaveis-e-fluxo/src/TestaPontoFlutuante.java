51f1fa8a8353925008fd79830b15a9f01c98c5c3
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2022-03-08 13:41:25 -0300

    Escreva seu primeiro código com eclipse

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,22 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		
+		System.out.println("O salário é: "+salario);
+
+		double divisao = 3.14 /2;
+		
+		System.out.println(divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao); // inteiro com inteiro da inteiro pro Java.
+		
+		double novaTentativa = 5/2; // guarda inteiro, porque 5 e 2 dão inteiro.
+		System.out.println(novaTentativa);
+				
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,24 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ola novo teste");
+		
+		int idade=25;
+		
+		System.out.println(idade);
+		
+		idade = 25+10;
+		
+		System.out.println(idade);
+		
+		idade = (7*5)+2;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é: "+idade+". Parabéns.");
+
+	}
+
+}
