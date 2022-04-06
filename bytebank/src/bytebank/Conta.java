ca10ec0da0e1245af5c332262e0ed4617c389f9c
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2022-04-06 14:29:15 -0300

    sessão 3 do curso

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -8,4 +8,31 @@ public class Conta {
 	int numero;
 	String titular;
 
+	/* criando ações para a nossa conta. */
+	/* metodos */
+
+	public void deposita(double valor) { /* void indica que não tem retorno */
+		this.saldo += valor; /* this indica "desta" faz a referencia no objeto */
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean tranfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+
 }
---
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -0,0 +1,32 @@
+package bytebank;
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		
+		conta.saldo=100;
+		
+		conta.deposita(50);
+		
+		System.out.println(conta.saldo);
+		
+		boolean conseguiuRetirar = conta.saca(20);
+		System.out.println(conta.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta conta2 = new Conta();
+		conta2.deposita(1000);
+		
+		boolean sucessoTransferencia = conta2.tranfere(300, conta);
+		if(sucessoTransferencia){
+			System.out.println("Transferencia com sucesso.");
+		} else {
+			System.out.println("faltou dinheiro!");
+		}
+		System.out.println(conta2.saldo);
+		System.out.println(conta.saldo);
+
+	}
+
+}
