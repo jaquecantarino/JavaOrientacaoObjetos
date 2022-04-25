159e0c9d7db4fa3c01815bb86109d2461ec32eea
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2022-04-24 22:30:43 -0300

    Herença

---
+++ bytebank-herdado/src/bytebank/Designer.java
@@ -0,0 +1,10 @@
+package bytebank;
+
+public class Designer extends Funcionario {
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do Designer");
+		return 200;
+	}
+
+}
