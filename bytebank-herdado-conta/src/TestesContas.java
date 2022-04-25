021bcfaf2d01942ab3b53a2d610ab65aa1643e92
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2022-04-24 22:41:47 -0300

    aula 4 polimorfismo

--- bytebank-herdado-conta/src/TestesContas.java
+++ bytebank-herdado-conta/src/TestesContas.java
@@ -13,6 +13,16 @@ public class TestesContas {
 		System.out.println(conta.getAgencia());
 
 		System.out.println("o total de contas é : " + Conta.getTotal());
+		
+		ContaCorrente cc = new ContaCorrente(111,111);
+        cc.deposita(100.0);
+
+        ContaPoupanca cp = new ContaPoupanca(222,222);
+        cp.deposita(200.0);
+
+        cc. transfere(10.0, cp);
+        System.out.println("CC: " + cc.getSaldo());
+        System.out.println("CP: " + cp.getSaldo());
 	}
 
 }
