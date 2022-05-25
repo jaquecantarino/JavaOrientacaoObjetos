69228669c550a875c3da8770c89dc7f83f77b25b
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2022-05-24 23:43:07 -0300

    Enums - Extra

---
+++ Enum/src/teste.java
@@ -0,0 +1,17 @@
+
+public class teste {
+
+	public static void main(String[] args) {
+		
+		Prioridade pMin = Prioridade.MIN;
+		Prioridade pMax = Prioridade.MAX;
+		
+
+		System.out.println(pMin.name());
+		System.out.println(pMax.name());
+		
+		System.out.println(pMin.ordinal());
+		System.out.println(pMax.ordinal());
+	}
+
+}
